b2f0811fda79a3c3b30d52dae9b5c4633b258cbb
Mike Harris <mikeharris.mailbox@gmail.com>
2016-12-28 11:24:05 -0600

    12/28/2016 Commit
    SteamVR added, script added

---
+++ CustomTiltBrush/Assets/DrawLineManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLineManager : MonoBehaviour {
+    public SteamVR_TrackedObject trackedObj;
+    private LineRenderer currLine;
+
+    // Update is called once per frame
+    void Update()
+    {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            GameObject go = new GameObject();
+            currLine = go.AddComponent<LineRenderer>();
+
+        }
+    }
+}
